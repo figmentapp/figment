ed5e98687872dbe6bf4f0b4b24728c13a803caa3
Frederik De Bleser <frederik@debleser.be>
2020-01-13 15:55:23 +0100

    Beter tailwind css config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,8 @@
-const tailwindcss = require('tailwindcss');
-const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
-const plugins = [tailwindcss];
-
-if (!IS_DEVELOPMENT) {
-  const purgecss = require('@fullhuman/postcss-purgecss');
-
-  class TailwindExtractor {
-    static extract(content) {
-      return content.match(/[A=Z0-9-:\/]+/g) || [];
-    }
-  }
-
-  plugins.push(
-    prugecss({
-      content: ['src/*.html'],
-      extractors: [
-        {
-          extractor: TailwindExtractor,
-          extensions: ['html']
-        }
-      ]
-    })
-  );
-}
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./index.html', './js/**/*.js'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
 
 module.exports = {
-  plugins
+  plugins: [require('tailwindcss'), ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])]
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    cursor: {
+      'ew-resize': 'ew-resize'
+    }
+  },
+  variants: {},
+  plugins: []
+};
