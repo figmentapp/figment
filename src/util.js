11354f35678d0b4b8e8e555018d524d04aee84cd
Frederik De Bleser <frederik@debleser.be>
2020-02-18 15:03:20 +0100

    Remove function that is now in lodash.

--- src/util.js
+++ src/util.js
@@ -1,11 +1,3 @@
 export function clamp(v, min, max) {
   return v < min ? min : v > max ? max : v;
 }
-
-// From https://stackoverflow.com/a/2970667
-export function toCamelCase(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-    if (+match === 0) return '';
-    return index == 0 ? match.toLowerCase() : match.toUpperCase();
-  });
-}
