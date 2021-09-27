a897c7faf02d9f9f8b586b3ceb630cca5c85f4c8
Lievn Menschaert <lievenmenschaert@gmail.com>
2021-09-27 14:09:59 +0200

    add wrap to circle shader

--- src/model/sources.js
+++ src/model/sources.js
@@ -1461,6 +1461,60 @@ imageIn.onChange = render;
 thresholdIn.onChange = render;
 `;
 
+image.wrap = `// Circular wrap of input image.
+
+const fragmentShader = \`
+precision mediump float;
+uniform sampler2D u_input_texture;
+uniform float u_radius;
+uniform float u_twist;
+varying vec2 v_uv;
+
+void main() {
+  vec2 uv = v_uv;
+ vec2 p = -1.0 + 2.0 * uv.st;
+ float r = sqrt(dot(p,p));
+
+p.x = mod(p.x + r * u_twist, 1.0);
+ float a = atan(p.y,p.x);
+
+uv.x = (a + 3.14159265359)/6.28318530718;
+uv.y = r / sqrt(u_radius);
+ vec3 col = texture2D(u_input_texture, uv).rgb;
+ gl_FragColor = vec4(col, 1.0);
+
+}
+\`;
+
+const imageIn = node.imageIn('in');
+const radiusIn = node.numberIn('radius', 2.0, { min: 0, max: 5, step: 0.01 });
+const twistIn = node.numberIn('twist', 0.0, { min: -1, max: 1, step: 0.01 });
+const imageOut = node.imageOut('out');
+
+let program, framebuffer;
+
+node.onStart = (props) => {
+  program = figment.createShaderProgram(fragmentShader);
+  framebuffer = new figment.Framebuffer();
+};
+
+function render() {
+  if (!imageIn.value) return;
+  if (!program) return;
+  if (!framebuffer) return;
+  framebuffer.setSize(imageIn.value.width, imageIn.value.height);
+  framebuffer.bind();
+  figment.drawQuad(program, { u_input_texture: imageIn.value.texture,u_radius: radiusIn.value,u_twist: twistIn.value });
+  framebuffer.unbind();
+  imageOut.set(framebuffer);
+}
+
+imageIn.onChange = render;
+radiusIn.onChange = render;
+twistIn.onChange = render;
+`;
+
+
 ml.classifyImage = `// Classify an image.
 // const ml5 = require('ml5');
 const imageIn = node.imageIn('image');
