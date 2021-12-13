ae75295a81eb58c07940b5da5f1388c988a085ca
Frederik De Bleser <frederik@debleser.be>
2021-12-13 13:50:32 +0100

    Try segment pose 2 node based on bodypix.

--- src/model/Library.js
+++ src/model/Library.js
@@ -74,6 +74,7 @@ export default class Library {
     this.nodeTypes.push({ name: 'Detect Objects', type: 'ml.detectObjects', source: ml.detectObjects });
     this.nodeTypes.push({ name: 'Detect Pose', type: 'ml.detectPose', source: ml.detectPose });
     this.nodeTypes.push({ name: 'Segment Pose', type: 'ml.segmentPose', source: ml.segmentPose });
+    this.nodeTypes.push({ name: 'Segment Pose 2', type: 'ml.segmentPose2', source: ml.segmentPose2 });
     // this.nodeTypes.push({ name: 'Classify Image', type: 'ml.classifyImage', source: ml.classifyImage });
     // this.nodeTypes.push({ name: 'Pose Net', type: 'ml.poseNet', source: ml.poseNet });
     // this.nodeTypes.push({ name: 'Pose Body Part', type: 'ml.poseBodyPart', source: ml.poseBodyPart });
