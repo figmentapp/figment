bff8d5adc3e4c2ab6e8a8357fd2227321f442fef
Frederik De Bleser <frederik@debleser.be>
2022-01-27 15:18:17 +0100

    Add a border node.

--- src/model/Library.js
+++ src/model/Library.js
@@ -43,6 +43,7 @@ export default class Library {
     // this.nodeTypes.push({ name: 'Draw Image', type: 'image.drawImage', source: image.drawImage });
     // this.nodeTypes.push({ name: 'Pixels', type: 'image.pixels', source: image.pixels });
     this.nodeTypes.push({ name: 'Blur', type: 'image.blur', source: image.blur });
+    this.nodeTypes.push({ name: 'Border', type: 'image.border', source: image.border });
     this.nodeTypes.push({ name: 'Composite', type: 'image.composite', source: image.composite });
     this.nodeTypes.push({ name: 'Constant', type: 'image.constant', source: image.constant });
     this.nodeTypes.push({ name: 'Crop', type: 'image.crop', source: image.crop });
--- src/model/sources.js
+++ src/model/sources.js
@@ -74,6 +74,59 @@ imageIn.onChange = render;
 blurIn.onChange = render;
 `;
 
+image.border = `// Generate a border around the image.
+
+const fragmentShader = \`
+precision mediump float;
+uniform sampler2D u_input_texture;
+uniform vec2 u_resolution;
+uniform vec4 u_border_color;
+uniform float u_border_size;
+varying vec2 v_uv;
+
+void main() {
+  float image_ratio = u_resolution.x / u_resolution.y;
+  float border_frac = u_border_size / u_resolution.x;
+  if (v_uv.x < border_frac || v_uv.x > 1.0 - border_frac || v_uv.y < border_frac || v_uv.y > 1.0 - border_frac) {
+    gl_FragColor = u_border_color;
+  } else {
+    gl_FragColor = texture2D(u_input_texture, v_uv);
+  }
+}
+\`
+
+const imageIn = node.imageIn('in');
+const borderSize = node.numberIn('borderSize', 10.0, { min: 1, max: 512, step: 1 });
+const borderColor = node.colorIn('borderColor', [255, 255, 255, 1.0]);
+const imageOut = node.imageOut('out');
+
+let program, framebuffer;
+
+node.onStart = (props) => {
+  program = figment.createShaderProgram(fragmentShader);
+  framebuffer = new figment.Framebuffer();
+};
+
+function render() {
+  if (!imageIn.value) return;
+  framebuffer.setSize(imageIn.value.width, imageIn.value.height);
+  framebuffer.bind();
+  figment.clear();
+  figment.drawQuad(program, { 
+    u_input_texture: imageIn.value.texture,
+    u_resolution: [imageIn.value.width, imageIn.value.height],
+    u_border_size: borderSize.value,
+    u_border_color: [borderColor.value[0] / 255, borderColor.value[1] / 255, borderColor.value[2] / 255, borderColor.value[3]]
+  });
+  framebuffer.unbind();
+  imageOut.set(framebuffer);
+}
+
+imageIn.onChange = render;
+borderSize.onChange = render;
+borderColor.onChange = render;
+`;
+
 image.composite = `// Combine two images together.
 
 const image1In = node.imageIn('image 1');
