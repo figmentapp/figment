fdc946ed1d208c82e10546c2908c30ff370033ac
Lievn Menschaert <lievenmenschaert@gmail.com>
2022-02-08 15:24:48 +0100

    add parameters for canny edges shader

--- src/model/sources.js
+++ src/model/sources.js
@@ -128,14 +128,14 @@ borderColor.onChange = render;
 `;
 
 image.canny = `// canny edge detection on input image.
-// https://www.shadertoy.com/view/sdcSz2
+
 const fragmentShader = \`
 precision mediump float;
 uniform sampler2D u_input_texture;
 uniform vec2 u_resolution;
+uniform float u_thickness;
+uniform float u_factor;
 varying vec2 v_uv;
-
-#define tickness 2.
 
 float getAve(vec2 uv){
     vec3 rgb = texture2D(u_input_texture, uv).rgb;
@@ -143,25 +143,20 @@ float getAve(vec2 uv){
     return dot(lum, rgb);
 }
 
-// Detect edge.
 vec4 sobel(vec2 fragCoord, vec2 dir){
-    vec2 uv = fragCoord;
+    vec2 uv2 = v_uv.xy;
     vec2 texel = 1./u_resolution.xy;
-    float np = getAve(uv + (vec2(-1,+1) + dir ) * texel * tickness);
-    float zp = getAve(uv + (vec2( 0,+1) + dir ) * texel * tickness);
-    float pp = getAve(uv + (vec2(+1,+1) + dir ) * texel * tickness);
+    float np = getAve(uv2 + (vec2(-1,+1) + dir ) * texel * u_thickness);
+    float zp = getAve(uv2 + (vec2( 0,+1) + dir ) * texel * u_thickness);
+    float pp = getAve(uv2 + (vec2(+1,+1) + dir ) * texel * u_thickness);
     
-    float nz = getAve(uv + (vec2(-1, 0) + dir ) * texel * tickness);
+    float nz = getAve(uv2 + (vec2(-1, 0) + dir ) * texel * u_thickness);
     // zz = 0
-    float pz = getAve(uv + (vec2(+1, 0) + dir ) * texel * tickness);
+    float pz = getAve(uv2 + (vec2(+1, 0) + dir ) * texel * u_thickness);
     
-    float nn = getAve(uv + (vec2(-1,-1) + dir ) * texel * tickness);
-    float zn = getAve(uv + (vec2( 0,-1) + dir ) * texel * tickness);
-    float pn = getAve(uv + (vec2(+1,-1) + dir ) * texel * tickness);
-    
-    // np zp pp
-    // nz zz pz
-    // nn zn pn
+    float nn = getAve(uv2 + (vec2(-1,-1) + dir ) * texel * u_thickness);
+    float zn = getAve(uv2 + (vec2( 0,-1) + dir ) * texel * u_thickness);
+    float pn = getAve(uv2 + (vec2(+1,-1) + dir ) * texel * u_thickness);
     
     #if 0
     float gx = (np*-1. + nz*-2. + nn*-1. + pp*1. + pz*2. + pn*1.);
@@ -173,18 +168,15 @@ vec4 sobel(vec2 fragCoord, vec2 dir){
     #endif
     
     vec2 G = vec2(gx,gy);
-    
     float grad = length(G);
-    
     float angle = atan(G.y, G.x);
     
     return vec4(G, grad, angle);
 }
 
-// Make edge thinner.
 vec2 hysteresisThr(vec2 fragCoord, float mn, float mx){
 
-    vec4 edge = sobel(fragCoord, vec2(0));
+    vec4 edge = sobel(fragCoord, vec2(0.0));
 
     vec2 dir = vec2(cos(edge.w), sin(edge.w));
     dir *= vec2(-1,1); // rotate 90 degrees.
@@ -202,35 +194,31 @@ vec2 hysteresisThr(vec2 fragCoord, float mn, float mx){
 
 float cannyEdge(vec2 fragCoord, float mn, float mx){
 
-    vec2 np = hysteresisThr(fragCoord + vec2(-1,+1), mn, mx);
-    vec2 zp = hysteresisThr(fragCoord + vec2( 0,+1), mn, mx);
-    vec2 pp = hysteresisThr(fragCoord + vec2(+1,+1), mn, mx);
+    vec2 np = hysteresisThr(fragCoord + vec2(-1.,+1.), mn, mx);
+    vec2 zp = hysteresisThr(fragCoord + vec2( 0.,+1.), mn, mx);
+    vec2 pp = hysteresisThr(fragCoord + vec2(+1.,+1.), mn, mx);
     
-    vec2 nz = hysteresisThr(fragCoord + vec2(-1, 0), mn, mx);
-    vec2 zz = hysteresisThr(fragCoord + vec2( 0, 0), mn, mx);
-    vec2 pz = hysteresisThr(fragCoord + vec2(+1, 0), mn, mx);
+    vec2 nz = hysteresisThr(fragCoord + vec2(-1., 0.), mn, mx);
+    vec2 zz = hysteresisThr(fragCoord + vec2( 0., 0.), mn, mx);
+    vec2 pz = hysteresisThr(fragCoord + vec2(+1., 0.), mn, mx);
     
-    vec2 nn = hysteresisThr(fragCoord + vec2(-1,-1), mn, mx);
-    vec2 zn = hysteresisThr(fragCoord + vec2( 0,-1), mn, mx);
-    vec2 pn = hysteresisThr(fragCoord + vec2(+1,-1), mn, mx);
+    vec2 nn = hysteresisThr(fragCoord + vec2(-1.,-1.), mn, mx);
+    vec2 zn = hysteresisThr(fragCoord + vec2( 0.,-1.), mn, mx);
+    vec2 pn = hysteresisThr(fragCoord + vec2(+1.,-1.), mn, mx);
     
-    // np zp pp
-    // nz zz pz
-    // nn zn pn
-    //return min(1., step(1e-3, zz.x) * (zp.y + nz.y + pz.y + zn.y)*8.);
-    //return min(1., step(1e-3, zz.x) * (np.y + pp.y + nn.y + pn.y)*8.);
-    return min(1., step(1e-2, zz.x*8.) * smoothstep(.0, .3, np.y + zp.y + pp.y + nz.y + pz.y + nn.y + zn.y + pn.y)*8.);
+    return min(1., step(1e-3, zz.x) * (zp.y + nz.y + pz.y + zn.y)*8.);
 }
 
 void main(){
     vec2 uv = v_uv;
-    float edge = cannyEdge(uv,2., 1.);   
+    float edge = cannyEdge(uv.xy, u_factor, u_factor);   
     gl_FragColor = vec4(vec3(1.-edge),1.0);
 }
-
 \`;
 
 const imageIn = node.imageIn('in');
+const thicknessIn = node.numberIn('thickness', 1.5, { min: 0.0, max: 10.0, step: 0.1 });
+const factorIn = node.numberIn('factor', 3., { min: 0.0, max: 10.0, step: 0.1 });
 const imageOut = node.imageOut('out');
 
 let program, framebuffer;
@@ -248,12 +236,16 @@ function render() {
   figment.drawQuad(program, { 
     u_input_texture: imageIn.value.texture,
     u_resolution: [imageIn.value.width, imageIn.value.height],
+    u_thickness: thicknessIn.value,
+    u_factor: factorIn.value
   });
   framebuffer.unbind();
   imageOut.set(framebuffer);
 }
 
 imageIn.onChange = render;
+thicknessIn.onChange = render;
+factorIn.onChange = render;
 `;
 
 image.composite = `// Combine two images together.
