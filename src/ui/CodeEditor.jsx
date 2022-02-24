dcdff027893d56ecba3838fc3afba2175ab20b18
Frederik De Bleser <frederik@debleser.be>
2022-02-23 23:30:23 -0500

    Use defaultValue to get rid of warning.

--- src/ui/CodeEditor.jsx
+++ src/ui/CodeEditor.jsx
@@ -50,7 +50,7 @@ export default class CodeEditor extends Component {
     return (
       <div className="code flex-grow flex flex-col overflow-hidden">
         <div className={'h-full w-full overflow-hidden ' + (readOnly ? 'opacity-50' : '')}>
-          <textarea className="code__area" id="code" value={this.state.source} readOnly={readOnly} />
+          <textarea className="code__area" id="code" defaultValue={this.state.source} readOnly={readOnly} />
         </div>
         <div className="code__actions px-4 py-3 flex items-center justify-between bg-gray-900">
           {readOnly && (
