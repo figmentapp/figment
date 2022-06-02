fdb726687cb8794f083528fe031274dab601802a
Frederik De Bleser <frederik@debleser.be>
2022-06-02 16:46:09 +0200

    Bundle a version of tensorflow.js.

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -4,12 +4,14 @@ import { WebGLRenderer } from 'three';
 import * as g from '../g';
 import * as figment from '../figment';
 import * as THREE from 'three';
+import * as tf from '@tensorflow/tfjs';
 
 import App from './App';
 
 window.g = g;
 window.THREE = THREE;
 window.figment = figment;
+window.tf = tf;
 ReactDOM.render(
   <React.StrictMode>
     <App />
