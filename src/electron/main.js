76afd64669365aa1a3d15837edf3c587d7784dfa
Frederik De Bleser <frederik@debleser.be>
2022-06-14 16:51:47 +0200

    Hide menu bar in fullscreen mode.

--- src/electron/main.js
+++ src/electron/main.js
@@ -148,6 +148,7 @@ ipcMain.handle('showNodeContextMenu', showNodeContextMenu);
 
 function setFullScreen(_, fullscreen) {
   gMainWindow.setFullScreen(fullscreen);
+  gMainWindow.setMenuBarVisibility(!fullscreen);
 }
 ipcMain.handle('setFullScreen', setFullScreen);
 
