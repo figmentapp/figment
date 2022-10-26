403932a7e699f017709f86915b3283b60c658b5c
Frederik De Bleser <frederik@debleser.be>
2022-10-26 16:12:00 +0200

    Add color dodge/burn blending modes.

--- src/model/sources.js
+++ src/model/sources.js
@@ -283,7 +283,7 @@ image.composite = `// Combine two images together.
 const image1In = node.imageIn('image 1');
 const image2In = node.imageIn('image 2');
 const factorIn = node.numberIn('factor', 0.5, { min: 0, max: 1, step: 0.01 });
-const operationIn = node.selectIn('operation', ['normal', 'darken', 'multiply', '---', 'lighten', 'screen', '---', 'difference'], 'normal');
+const operationIn = node.selectIn('operation', ['normal', 'darken', 'multiply', 'color burn', '---', 'lighten', 'screen', 'color dodge', '---', 'difference'], 'normal');
 const imageOut = node.imageOut('out');
 
 function updateShader() {
@@ -294,14 +294,18 @@ function updateShader() {
     blendFunction = 'factor * vec3(min(c1.r, c2.r), min(c1.g, c2.g), min(c1.b, c2.b)) + (1.0 - factor) * c1.rgb';
   } else if (operationIn.value === 'multiply') {
     blendFunction = 'factor * (c1.rgb * c2.rgb) + (1.0 - factor) * c1.rgb';
+  } else if (operationIn.value === 'color burn') {
+    blendFunction = 'factor * vec3(blendColorBurn(c1.r,c2.r),blendColorBurn(c1.g,c2.g),blendColorBurn(c1.b,c2.b)) + (1.0 - factor) * c1.rgb';
   } else if (operationIn.value === 'lighten') {
     blendFunction = 'factor * vec3(max(c1.r, c2.r), max(c1.g, c2.g), max(c1.b, c2.b)) + (1.0 - factor) * c1.rgb';
   } else if (operationIn.value === 'screen') {
     blendFunction = 'factor * vec3(blendScreen(c1.r, c2.r), blendScreen(c1.g, c2.g), blendScreen(c1.b, c2.b))';
+  } else if (operationIn.value === 'color dodge') {
+    blendFunction = 'factor * vec3(blendColorDodge(c1.r, c2.r), blendColorDodge(c1.g, c2.g), blendColorDodge(c1.b, c2.b))';
   } else if (operationIn.value === 'difference') {
     blendFunction = 'factor * abs(c1.rgb - c2.rgb) + (1.0 - factor) * c1.rgb';
   } else {
-    throw new Error(\`Unknown operation: \${operationIn.value}\`);
+    blendFunction = 'factor * c2.rgb + (1.0 - factor) * c1.rgb';
   }
   const fragmentShader = \`
   precision mediump float;
@@ -309,7 +313,9 @@ function updateShader() {
   uniform sampler2D u_image_2;
   uniform float u_factor;
   varying vec2 v_uv;
-  float blendScreen(float c1, float c2) { return 1.0-((1.0-c1)*(1.0-c2)); }  
+  float blendColorBurn(float c1, float c2) { return (c2==0.0)?c2:max((1.0-((1.0-c1)/c2)),0.0); }
+  float blendScreen(float c1, float c2) { return 1.0-((1.0-c1)*(1.0-c2)); }
+  float blendColorDodge(float c1, float c2) { return (c2==1.0)?c2:min(c1/(1.0-c2),1.0); }
   void main() {
     vec4 c1 = texture2D(u_image_1, v_uv);
     vec4 c2 = texture2D(u_image_2, v_uv);
