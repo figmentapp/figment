90a7760137b30892136d9b30e2f7bc0762ba1b95
Lievn Menschaert <lievenmenschaert@gmail.com>
2023-03-17 17:08:57 +0100

    add color key node

--- src/model/Library.js
+++ src/model/Library.js
@@ -51,6 +51,7 @@ export default class Library {
     this.nodeTypes.push({ name: 'Brannan', type: 'image.brannan', source: image.brannan });
     this.nodeTypes.push({ name: 'Canny Edges', type: 'image.canny', source: image.canny });
     this.nodeTypes.push({ name: 'Cartoon', type: 'image.cartoon', source: image.cartoon });
+    this.nodeTypes.push({ name: 'Color Key', type: 'image.colorKey', source: image.colorKey });
     this.nodeTypes.push({ name: 'Composite', type: 'image.composite', source: image.composite });
     this.nodeTypes.push({ name: 'Constant', type: 'image.constant', source: image.constant });
     this.nodeTypes.push({ name: 'Crop', type: 'image.crop', source: image.crop });
--- src/model/sources.js
+++ src/model/sources.js
@@ -500,6 +500,57 @@ node.onRender = () => {
 };
 `;
 
+image.colorKey = `//  Color keying on input image.
+
+const fragmentShader = \`
+precision mediump float;
+uniform sampler2D u_input_texture;
+uniform vec3 u_keyColor; 
+uniform float u_threshold; 
+varying vec2 v_uv;
+void main() {
+
+  vec2 uv = v_uv;
+  vec4 color = texture2D(u_input_texture, uv.st); 
+  
+  // calculate the color difference between the current pixel and the key color
+  float difference = length(color.rgb - u_keyColor);
+  
+  // if the difference is less than the threshold, set the alpha to 0
+  if (difference < u_threshold) {
+    color.a = 0.0;
+  }
+  
+  gl_FragColor = color;
+}
+\`;
+
+const imageIn = node.imageIn('in');
+const colorIn = node.colorIn('key color', [128, 128, 128, 1.0]);
+const thresholdIn = node.numberIn('threshold', 0.4, { min: 0.0, max: 1.0, step: 0.01 });
+const imageOut = node.imageOut('out');
+
+let program, framebuffer;
+
+node.onStart = (props) => {
+  program = figment.createShaderProgram(fragmentShader);
+  framebuffer = new figment.Framebuffer();
+};
+
+node.onRender = () => {
+  if (!imageIn.value) return;
+  framebuffer.setSize(imageIn.value.width, imageIn.value.height);
+  framebuffer.bind();
+  figment.clear();  
+  figment.drawQuad(program, { u_input_texture: imageIn.value.texture,
+    u_keyColor: [colorIn.value[0] / 255, colorIn.value[1] / 255, colorIn.value[2] / 255, colorIn.value[3]],
+    u_threshold: thresholdIn.value,
+   });
+  framebuffer.unbind();
+  imageOut.set(framebuffer);
+};
+`;
+
 image.composite = `// Combine two images together.
 
 const image1In = node.imageIn('image 1');
