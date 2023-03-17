b3beef6394b20447f4f28fdf337eb2877fa3a409
Lievn Menschaert <lievenmenschaert@gmail.com>
2023-03-17 16:44:34 +0100

    add color glitch node

--- src/model/Library.js
+++ src/model/Library.js
@@ -57,6 +57,7 @@ export default class Library {
     this.nodeTypes.push({ name: 'Distortion', type: 'image.distortion', source: image.distortion });
     this.nodeTypes.push({ name: 'Emboss', type: 'image.emboss', source: image.emboss });
     this.nodeTypes.push({ name: 'Graussian Blur', type: 'image.gaussianBlur', source: image.gaussianBlur });
+    this.nodeTypes.push({ name: 'Glitch', type: 'image.glitch', source: image.glitch });
     this.nodeTypes.push({ name: 'Grayscale', type: 'image.grayscale', source: image.grayscale });
     this.nodeTypes.push({ name: 'Instagram Filters', type: 'image.instagram', source: image.instagram });
     this.nodeTypes.push({ name: 'Invert', type: 'image.invert', source: image.invert });
