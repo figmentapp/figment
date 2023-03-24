7dda03c447c8e2962a2917c0f8a2487997db6289
Lievn Menschaert <lievenmenschaert@gmail.com>
2023-03-24 08:33:25 +0100

    add screen distortion node

--- src/model/Library.js
+++ src/model/Library.js
@@ -88,6 +88,7 @@ export default class Library {
     this.nodeTypes.push({ name: 'Resize', type: 'image.resize', source: image.resize });
     this.nodeTypes.push({ name: 'Rgb color clustering', type: 'image.rgbColorCLustering', source: image.rgbColorCLustering });
     this.nodeTypes.push({ name: 'Save Image', type: 'image.saveImage', source: image.saveImage });
+    this.nodeTypes.push({ name: 'Screen Distortion', type: 'image.screendistortion', source: image.screendistortion });
     this.nodeTypes.push({ name: 'Sepia', type: 'image.sepia', source: image.sepia });
     this.nodeTypes.push({ name: 'Sharpen', type: 'image.sharpen', source: image.sharpen });
     this.nodeTypes.push({ name: 'Sobel', type: 'image.sobel', source: image.sobel });
