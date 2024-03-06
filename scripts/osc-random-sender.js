005a1c87db36d4cb0c7c38772a51fa632824ae73
Frederik De Bleser <frederik@debleser.be>
2024-03-06 09:47:30 +0100

    Change OSC sender port to match receiving port.

--- scripts/osc-random-sender.js
+++ scripts/osc-random-sender.js
@@ -3,7 +3,7 @@ import { Client } from 'node-osc';
 function main() {
   setInterval(() => {
     if (Math.random() > 0.5) {
-      const client = new Client('127.0.0.1', 8888);
+      const client = new Client('127.0.0.1', 8000);
       client.send('/alpha', Math.random() * 200, () => {
         client.close();
       });
