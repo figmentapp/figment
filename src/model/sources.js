2bd1524753a13b04752c6f9c7a14a990ac21f018
Frederik De Bleser <frederik@debleser.be>
2024-08-27 23:53:24 +0200

    Improve ONNX performance by providing own buffers and using graphCapture

--- src/model/sources.js
+++ src/model/sources.js
@@ -4893,9 +4893,12 @@ ml.onnxImageModel = `// Run a generative image to image model using ONNX Runtime
 const imageIn = node.imageIn('in');
 const modelFileIn = node.fileIn('model');
 const imageOut = node.imageOut('out');
-let oldModelFile, session, canvas, framebuffer, isRunning = false;
-let inputBuffer = new Float32Array(3 * 512 * 512);
-let outputBuffer = new Uint8Array(4 * 512 * 512);
+let oldModelFile, session, device, canvas, framebuffer, isRunning = false;
+const BUFFER_SIZE = 3 * 512 * 512 * 4;
+const inputArray = new Float32Array(3 * 512 * 512);
+const outputArray = new Float32Array(3 * 512 * 512);
+const textureBuffer = new Uint8Array(4 * 512 * 512);
+let inputBuffer, outputBuffer, stagingBuffer, inputTensor, outputTensor;
 
 node.onStart = async () => {
   canvas = new OffscreenCanvas(512, 512);
@@ -4906,7 +4909,13 @@ async function loadModel() {
   if (!modelFileIn.value) return;
   const modelUrl = figment.urlForAsset(modelFileIn.value);
   try {
-    session = await ort.InferenceSession.create(modelUrl, {  executionProviders: ['webgpu'] });
+    session = await ort.InferenceSession.create(modelUrl, { executionProviders: ['webgpu'], enableGraphCapture: true });
+    device = ort.env.webgpu.device;
+    inputBuffer = device.createBuffer({ usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST, size: BUFFER_SIZE });
+    outputBuffer = device.createBuffer({ usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC, size: BUFFER_SIZE });
+    stagingBuffer = device.createBuffer({ usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST, size: BUFFER_SIZE });
+    inputTensor = ort.Tensor.fromGpuBuffer(inputBuffer, { dataType: "float32", dims: [1, 3, 512, 512] });
+    outputTensor = ort.Tensor.fromGpuBuffer(outputBuffer, { dataType: "float32", dims: [1, 3, 512, 512] });
     oldModelFile = modelFileIn.value;
   } catch (e) {
     console.error("Failed to load ONNX model:", e);
@@ -4939,30 +4948,39 @@ node.onRender = async () => {
   let blueOffset = pixelCount * 2;
   for (let i = 0; i < pixelCount; i++) {
     const inOffset = i * 4;
-    inputBuffer[redOffset++] = imageData.data[inOffset] / 127.5 - 1;
-    inputBuffer[greenOffset++] = imageData.data[inOffset + 1] / 127.5 - 1;
-    inputBuffer[blueOffset++] = imageData.data[inOffset + 2] / 127.5 - 1;
+    inputArray[redOffset++] = imageData.data[inOffset] / 127.5 - 1;
+    inputArray[greenOffset++] = imageData.data[inOffset + 1] / 127.5 - 1;
+    inputArray[blueOffset++] = imageData.data[inOffset + 2] / 127.5 - 1;
   }
-  const inputTensor = new ort.Tensor('float32', inputBuffer, [1, 3, 512, 512]);
+  device.queue.writeBuffer(inputBuffer, 0, inputArray);
 
   // Run inference
-  const outputMap = await session.run({ 'input': inputTensor });
-  const outputData = outputMap['output'].data;
+  await session.run({ input: inputTensor }, { output: outputTensor });
 
-  // Convert output tensor to framebuffer
+  // Do some WebGPU magic to get the data from the output tensor to a Float32Array
+  const commandEncoder = device.createCommandEncoder();
+  commandEncoder.copyBufferToBuffer(outputBuffer, 0, stagingBuffer, 0, BUFFER_SIZE);
+  device.queue.submit([commandEncoder.finish()]);
+  await stagingBuffer.mapAsync(GPUMapMode.READ, 0, BUFFER_SIZE);
+  const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
+  outputArray.set(new Float32Array(copyArrayBuffer));
+  stagingBuffer.unmap();
+
+  // Convert the output array to framebuffer
   redOffset = 0;
   greenOffset = pixelCount;
   blueOffset = pixelCount * 2;
   for (let i = 0; i < pixelCount; i++) {
     const outOffset = i * 4;
-    outputBuffer[outOffset] = clamp(outputData[redOffset++] * 127.5 + 127.5);
-    outputBuffer[outOffset + 1] = clamp(outputData[greenOffset++] * 127.5 + 127.5);
-    outputBuffer[outOffset + 2] = clamp(outputData[blueOffset++] * 127.5 + 127.5);
-    outputBuffer[outOffset + 3] = 255;
+    textureBuffer[outOffset] = clamp(outputArray[redOffset++] * 127.5 + 127.5);
+    textureBuffer[outOffset + 1] = clamp(outputArray[greenOffset++] * 127.5 + 127.5);
+    textureBuffer[outOffset + 2] = clamp(outputArray[blueOffset++] * 127.5 + 127.5);
+    textureBuffer[outOffset + 3] = 255;
   }
+
   // Upload the RGBA data directly to the framebuffer's texture
   gl.bindTexture(gl.TEXTURE_2D, framebuffer.texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, outputBuffer);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, textureBuffer);
   gl.bindTexture(gl.TEXTURE_2D, null);
   imageOut.set(framebuffer);
   isRunning = false;
