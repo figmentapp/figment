5726ebea220c78a7ca525f9ee58b8ce01c8adf4f
Frederik De Bleser <frederik@debleser.be>
2025-06-20 10:36:33 +0200

    Update detect faces to new node

--- src/nodes/ml/detectFaces.js
+++ src/nodes/ml/detectFaces.js
@@ -1,111 +1,63 @@
 /**
  * @name Detect Faces
- * @description Detect faces in an image using FaceMesh
+ * @description Detect faces in an image using MediaPipe
  * @category ml
  */
 
 const imageIn = node.imageIn('in');
 const backgroundIn = node.colorIn('background', [0, 0, 0, 1]);
-const contoursToggleIn = node.toggleIn('draw contours', true);
-const contoursColorIn = node.colorIn('contours color', [255, 255, 255, 1]);
-const contoursLineWidthIn = node.numberIn('contours line width', 1, { min: 0, max: 10, step: 0.1 });
-const tesselationToggleIn = node.toggleIn('draw tesselation', false);
-const tesselationColorIn = node.colorIn('tesselation color', [255, 255, 255, 1]);
-const tesselationLineWidthIn = node.numberIn('tesselation line width', 1, { min: 0, max: 10, step: 0.1 });
-const bboxToggleIn = node.toggleIn('draw bounding box', false);
-const bboxColorIn = node.colorIn('bounding box color', [255, 255, 255, 1]);
-const bboxLineWidthIn = node.numberIn('bounding box line width', 1, { min: 0, max: 10, step: 0.1 });
-contoursColorIn.label = 'color';
-tesselationColorIn.label = 'color';
-bboxColorIn.label = 'color';
-contoursLineWidthIn.label = 'line width';
-tesselationLineWidthIn.label = 'line width';
-bboxLineWidthIn.label = 'line width';
+const drawModeIn = node.selectIn('draw mode', ['contours', 'tesselation', 'bounding box'], 'contours');
+const drawColorIn = node.colorIn('draw color', [255, 255, 255, 1]);
+const drawLineWidthIn = node.numberIn('line width', 1, { min: 0, max: 10, step: 0.1 });
+const numFacesIn = node.numberIn('number of faces', 1, { min: 1, max: 4, step: 1 });
+const confidenceIn = node.numberIn('confidence', 0.5, { min: 0, max: 1, step: 0.01 });
 
 const imageOut = node.imageOut('out');
 const detectedOut = node.booleanOut('detected');
 const landmarksOut = node.objectOut('landmarks');
 
-const FACE_STATE_INITIALIZING = 'INITIALIZING';
-const FACE_STATE_RUNNING = 'RUNNING';
+drawColorIn.label = 'Color';
+drawLineWidthIn.label = 'Line Width';
 
-let _faceMesh, _canvas, _ctx, _framebuffer, _imageData, _results, _isProcessing;
-let _faceState = FACE_STATE_INITIALIZING;
+let _vision, _faceLandmarker;
+let _framebuffer, _canvas, _ctx, _imageData;
+let _drawingUtils;
+let _initialising = false;
+
+async function initLandmarker() {
+  if (_initialising) return;
+  _initialising = true;
+  if (_faceLandmarker) {
+    await _faceLandmarker.close();
+  }
+
+  _faceLandmarker = await mediapipe.FaceLandmarker.createFromOptions(_vision, {
+    baseOptions: {
+      modelAssetPath: './new-mediapipe/face_landmarker.task',
+      delegate: 'GPU',
+    },
+    runningMode: 'IMAGE',
+    numFaces: numFacesIn.value,
+    minFaceDetectionConfidence: confidenceIn.value,
+    minFacePresenceConfidence: confidenceIn.value,
+    minTrackingConfidence: confidenceIn.value,
+    outputFaceBlendshapes: false,
+    outputFacialTransformationMatrixes: false,
+  });
+  _initialising = false;
+}
 
 node.onStart = async () => {
   _framebuffer = new figment.Framebuffer();
   _canvas = new OffscreenCanvas(1, 1);
   _ctx = _canvas.getContext('2d');
-  await figment.loadScripts(['./mediapipe/drawing_utils.js', './mediapipe/face_mesh.js']);
-
-  _initFaceMesh();
+  _drawingUtils = new mediapipe.DrawingUtils(_ctx);
+  _vision = await mediapipe.FilesetResolver.forVisionTasks('./new-mediapipe');
+  await initLandmarker();
 };
 
-async function _initFaceMesh() {
-  _faceState = FACE_STATE_INITIALIZING;
-
-  _faceMesh = new FaceMesh({
-    locateFile: (file) => {
-      return `./mediapipe/${file}`;
-    },
-  });
-  _faceMesh.setOptions({
-    maxNumFaces: 1,
-    refineLandmarks: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5,
-  });
-
-  await _faceMesh.initialize();
-  _faceState = FACE_STATE_RUNNING;
-  _isProcessing = false;
-}
-
-function _detect(image) {
-  // Check if only one image is processed at the same time.
-  if (_isProcessing || !_faceMesh) return;
-  return new Promise((resolve) => {
-    _isProcessing = true;
-    try {
-      _faceMesh.onResults((results) => {
-        _faceMesh.onResults(null);
-        _isProcessing = false;
-        resolve(results);
-      });
-      _faceMesh.send({ image }).catch((err) => _handleDetectionError(err));
-    } catch (err) {
-      console.error('Error in face detection:', err);
-      _isProcessing = false;
-      resolve(null);
-    }
-  });
-}
-
-function _handleDetectionError(error) {
-  console.error('Error in face detection:', error);
-
-  try {
-    _faceMesh?.onResults(null);
-  } catch {}
-  try {
-    _faceMesh?.close();
-  } catch {}
-
-  _faceMesh = null;
-  _faceState = FACE_STATE_INITIALIZING;
-  _isProcessing = false;
-  _results = null;
-
-  drawResults();
-  landmarksOut.set(null);
-  _initFaceMesh(); // restart
-}
-
-node.onRender = async () => {
-  if (!imageIn.value || imageIn.value.width === 0 || imageIn.value.height === 0) return;
-  if (!_faceMesh || _faceState !== FACE_STATE_RUNNING) return;
-
-  // Draw the image on an ImageData object.
+node.onRender = () => {
+  if (!imageIn.value || _initialising) return;
   const width = imageIn.value.width;
   const height = imageIn.value.height;
 
@@ -116,77 +68,68 @@ node.onRender = async () => {
     _framebuffer.setSize(width, height);
   }
 
-  // Video nodes pass along this extra object with the framebuffer.
-  // This allows mediapose to avoid reading the texture first from the framebuffer.
-  if (imageIn.value._directImageHack) {
-    _results = await _detect(imageIn.value._directImageHack);
-  } else {
-    imageIn.value.bind();
-    window.gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, _imageData.data);
-    imageIn.value.unbind();
-    _results = await _detect(_imageData);
-  }
-  drawResults();
-  landmarksOut.set(_results ? { type: 'face', landmarks: _results.multiFaceLandmarks } : null);
+  imageIn.value.bind();
+  window.gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, _imageData.data);
+  imageIn.value.unbind();
+
+  const faceResult = _faceLandmarker.detect(_imageData);
+  drawResults(faceResult);
 };
 
-function drawResults() {
-  if (!imageIn.value || imageIn.value.width === 0 || imageIn.value.height === 0) return;
-  if (!_results) return;
+function drawResults(faceResult) {
+  if (!imageIn.value || !faceResult) return;
   const width = imageIn.value.width;
   const height = imageIn.value.height;
+
   _ctx.clearRect(0, 0, width, height);
   _ctx.fillStyle = figment.toCanvasColor(backgroundIn.value);
   _ctx.fillRect(0, 0, width, height);
-  if (_results.multiFaceLandmarks) {
-    detectedOut.set(_results.multiFaceLandmarks.length > 0);
-    for (const landmarks of _results.multiFaceLandmarks) {
-      if (contoursToggleIn.value) {
-        drawConnectors(_ctx, landmarks, FACEMESH_CONTOURS, {
-          color: figment.toCanvasColor(contoursColorIn.value),
-          lineWidth: contoursLineWidthIn.value,
-        });
-      }
-      if (tesselationToggleIn.value) {
-        drawConnectors(_ctx, landmarks, FACEMESH_TESSELATION, {
-          color: figment.toCanvasColor(tesselationColorIn.value),
-          lineWidth: tesselationLineWidthIn.value,
-        });
-      }
-      if (bboxToggleIn.value) {
-        let minX, minY, maxX, maxY;
-        for (let i = 0; i < landmarks.length; i++) {
-          if (i === 0) {
-            minX = maxX = landmarks[i].x;
-            minY = maxY = landmarks[i].y;
-          } else {
-            minX = Math.min(minX, landmarks[i].x);
-            minY = Math.min(minY, landmarks[i].y);
-            maxX = Math.max(maxX, landmarks[i].x);
-            maxY = Math.max(maxY, landmarks[i].y);
+
+  if (faceResult.faceLandmarks && faceResult.faceLandmarks.length > 0) {
+    detectedOut.value = true;
+    landmarksOut.value = { type: 'face', landmarks: faceResult.faceLandmarks };
+
+    const options = {
+      color: figment.toCanvasColor(drawColorIn.value),
+      lineWidth: drawLineWidthIn.value,
+    };
+
+    for (const landmarks of faceResult.faceLandmarks) {
+      switch (drawModeIn.value) {
+        case 'contours':
+          _drawingUtils.drawConnectors(landmarks, mediapipe.FaceLandmarker.FACE_LANDMARKS_CONTOURS, options);
+          break;
+        case 'tesselation':
+          _drawingUtils.drawConnectors(landmarks, mediapipe.FaceLandmarker.FACE_LANDMARKS_TESSELATION, options);
+          break;
+        case 'bounding box':
+          let minX = Infinity,
+            minY = Infinity,
+            maxX = -Infinity,
+            maxY = -Infinity;
+          for (const landmark of landmarks) {
+            minX = Math.min(minX, landmark.x);
+            minY = Math.min(minY, landmark.y);
+            maxX = Math.max(maxX, landmark.x);
+            maxY = Math.max(maxY, landmark.y);
           }
-        }
-        _ctx.strokeStyle = figment.toCanvasColor(bboxColorIn.value);
-        _ctx.lineWidth = bboxLineWidthIn.value;
-        _ctx.strokeRect(minX * width, minY * height, (maxX - minX) * width, (maxY - minY) * height);
+
+          _ctx.strokeStyle = figment.toCanvasColor(drawColorIn.value);
+          _ctx.lineWidth = drawLineWidthIn.value;
+          _ctx.strokeRect(minX * width, minY * height, (maxX - minX) * width, (maxY - minY) * height);
+          break;
       }
-      //drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
     }
   } else {
-    console.log('no faces');
-    detectedOut.set(false);
+    detectedOut.value = false;
+    landmarksOut.value = null;
   }
+
   window.gl.bindTexture(gl.TEXTURE_2D, _framebuffer.texture);
   window.gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, _canvas);
   window.gl.bindTexture(gl.TEXTURE_2D, null);
-  imageOut.set(_framebuffer);
+  imageOut.value = _framebuffer;
 }
 
-// imageIn.onChange = detectFaces;
-// backgroundIn.onChange = drawResults;
-// tesselationToggleIn.onChange = drawResults;
-// tesselationColorIn.onChange = drawResults;
-// tesselationLineWidthIn.onChange = drawResults;
-// contoursToggleIn.onChange = drawResults;
-// contoursColorIn.onChange = drawResults;
-// contoursLineWidthIn.onChange = drawResults;
+numFacesIn.onChange = initLandmarker;
+confidenceIn.onChange = initLandmarker;
