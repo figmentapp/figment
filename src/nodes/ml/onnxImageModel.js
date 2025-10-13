149653d01e7f21f64c693a5d58498fa1471352be
Frederik De Bleser <frederik@debleser.be>
2025-10-13 11:32:11 +0200

    ONNX Image Model node: Wrap core execution in try/catch

--- src/nodes/ml/onnxImageModel.js
+++ src/nodes/ml/onnxImageModel.js
@@ -62,52 +62,55 @@ node.onRender = async () => {
 
   isRunning = true;
 
-  // Convert framebuffer to input tensor
-  imageIn.value.bind();
-  window.gl.readPixels(0, 0, 512, 512, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
-  imageIn.value.unbind();
-  const pixelCount = 512 * 512;
-  // ONNX expects images in NCHW format, so we need to have all channels after each other.
-  // In other words, first all red pixels, then all green pixels, and finally all blue pixels.
-  let redOffset = 0;
-  let greenOffset = pixelCount;
-  let blueOffset = pixelCount * 2;
-  for (let i = 0; i < pixelCount; i++) {
-    const inOffset = i * 4;
-    inputArray[redOffset++] = imageData[inOffset] / 127.5 - 1;
-    inputArray[greenOffset++] = imageData[inOffset + 1] / 127.5 - 1;
-    inputArray[blueOffset++] = imageData[inOffset + 2] / 127.5 - 1;
+  try {
+    // Convert framebuffer to input tensor
+    imageIn.value.bind();
+    window.gl.readPixels(0, 0, 512, 512, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
+    imageIn.value.unbind();
+    const pixelCount = 512 * 512;
+    // ONNX expects images in NCHW format, so we need to have all channels after each other.
+    // In other words, first all red pixels, then all green pixels, and finally all blue pixels.
+    let redOffset = 0;
+    let greenOffset = pixelCount;
+    let blueOffset = pixelCount * 2;
+    for (let i = 0; i < pixelCount; i++) {
+      const inOffset = i * 4;
+      inputArray[redOffset++] = imageData[inOffset] / 127.5 - 1;
+      inputArray[greenOffset++] = imageData[inOffset + 1] / 127.5 - 1;
+      inputArray[blueOffset++] = imageData[inOffset + 2] / 127.5 - 1;
+    }
+    device.queue.writeBuffer(inputBuffer, 0, inputArray);
+
+    // Run inference
+    await session.run({ input: inputTensor }, { output: outputTensor });
+
+    // Do some WebGPU magic to get the data from the output tensor to a Float32Array
+    const commandEncoder = device.createCommandEncoder();
+    commandEncoder.copyBufferToBuffer(outputBuffer, 0, stagingBuffer, 0, BUFFER_SIZE);
+    device.queue.submit([commandEncoder.finish()]);
+    await stagingBuffer.mapAsync(GPUMapMode.READ, 0, BUFFER_SIZE);
+    const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
+    outputArray.set(new Float32Array(copyArrayBuffer));
+    stagingBuffer.unmap();
+
+    // Convert the output array to framebuffer
+    redOffset = 0;
+    greenOffset = pixelCount;
+    blueOffset = pixelCount * 2;
+    for (let i = 0; i < pixelCount; i++) {
+      const outOffset = i * 4;
+      textureBuffer[outOffset] = clamp(outputArray[redOffset++] * 127.5 + 127.5);
+      textureBuffer[outOffset + 1] = clamp(outputArray[greenOffset++] * 127.5 + 127.5);
+      textureBuffer[outOffset + 2] = clamp(outputArray[blueOffset++] * 127.5 + 127.5);
+      textureBuffer[outOffset + 3] = 255;
+    }
+
+    // Upload the RGBA data directly to the framebuffer's texture
+    gl.bindTexture(gl.TEXTURE_2D, framebuffer.texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, textureBuffer);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    imageOut.set(framebuffer);
+  } finally {
+    isRunning = false;
   }
-  device.queue.writeBuffer(inputBuffer, 0, inputArray);
-
-  // Run inference
-  await session.run({ input: inputTensor }, { output: outputTensor });
-
-  // Do some WebGPU magic to get the data from the output tensor to a Float32Array
-  const commandEncoder = device.createCommandEncoder();
-  commandEncoder.copyBufferToBuffer(outputBuffer, 0, stagingBuffer, 0, BUFFER_SIZE);
-  device.queue.submit([commandEncoder.finish()]);
-  await stagingBuffer.mapAsync(GPUMapMode.READ, 0, BUFFER_SIZE);
-  const copyArrayBuffer = stagingBuffer.getMappedRange(0, BUFFER_SIZE);
-  outputArray.set(new Float32Array(copyArrayBuffer));
-  stagingBuffer.unmap();
-
-  // Convert the output array to framebuffer
-  redOffset = 0;
-  greenOffset = pixelCount;
-  blueOffset = pixelCount * 2;
-  for (let i = 0; i < pixelCount; i++) {
-    const outOffset = i * 4;
-    textureBuffer[outOffset] = clamp(outputArray[redOffset++] * 127.5 + 127.5);
-    textureBuffer[outOffset + 1] = clamp(outputArray[greenOffset++] * 127.5 + 127.5);
-    textureBuffer[outOffset + 2] = clamp(outputArray[blueOffset++] * 127.5 + 127.5);
-    textureBuffer[outOffset + 3] = 255;
-  }
-
-  // Upload the RGBA data directly to the framebuffer's texture
-  gl.bindTexture(gl.TEXTURE_2D, framebuffer.texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, textureBuffer);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-  imageOut.set(framebuffer);
-  isRunning = false;
 };
