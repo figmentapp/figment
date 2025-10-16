bb11ac21e3936f506b716396e97250461dbc31ad
Frederik De Bleser <frederik@debleser.be>
2025-10-16 22:11:36 +0200

    Avoid unfound resources returning HTML

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  appType: 'mpa',
   base: './',
   publicDir: './assets',
   plugins: [tailwindcss(), react()],
